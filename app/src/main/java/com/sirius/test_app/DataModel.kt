5f96bf95c94a9157a151ac9918c89060ca94a2a5
whitequartz <nadim.abdulgani@mail.ru>
2022-07-15 15:13:41 +0300

    Верстка старницы

--- app/src/main/java/com/sirius/test_app/DataModel.kt
+++ app/src/main/java/com/sirius/test_app/DataModel.kt
@@ -1,8 +1,8 @@
 package com.sirius.test_app
 
 class DataModel(
-    val image: String = "https://ibb.co/37KzF03",
-    val logo: String = "https://ibb.co/GpbZg5t",
+    val image: String = "https://i.ibb.co/g3YVWD2/img-background.png",
+    val logo: String = "https://i.ibb.co/Pjf2B69/img-logo.png",
     val name: String = "DoTA 2",
     val rating: Float = 4.9f,
     val gradeCnt: String = "70M",
@@ -14,13 +14,13 @@ class DataModel(
     val description: String = "Dota 2 is a multiplayer online battle arena (MOBA) game which has two teams of five players compete to collectively destroy a large structure defended by the opposing team known as the \"Ancient\", whilst defending their own.",
     val reviews: List<ReviewModel> = listOf(
         ReviewModel(
-            userImage = "https://ibb.co/WcJMjSw",
+            userImage = "https://i.ibb.co/55HSwqy/img-user-1.png",
             userName = "Auguste Conte",
             date = "February 14, 2019",
             message = "“Once you start to learn its secrets, there’s a wild and exciting variety of play here that’s unmatched, even by its peers.”"
         ),
         ReviewModel(
-            userImage = "https://ibb.co/p1q4QZr",
+            userImage = "https://i.ibb.co/j8t3zGZ/img-user-2.png",
             userName = "Jang Marcelino",
             date = "February 14, 2019",
             message =
--- app/src/main/java/com/sirius/test_app/MainActivity.kt
+++ app/src/main/java/com/sirius/test_app/MainActivity.kt
@@ -1,11 +1,74 @@
 package com.sirius.test_app
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Priority
+import com.nex3z.flowlayout.FlowLayout
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor", "InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dataModel = DataModel()
+
+        val imageApp = findViewById<ImageView>(R.id.imageApp)
+        val iconApp = findViewById<ImageView>(R.id.iconApp)
+        val nameApp = findViewById<TextView>(R.id.nameApp)
+        val ratingAppUpper = findViewById<RatingBar>(R.id.ratingAppUpper)
+        val gradeCount = findViewById<TextView>(R.id.gradeCount)
+        val tagsList = findViewById<FlowLayout>(R.id.tagsList)
+        val descApp = findViewById<TextView>(R.id.descApp)
+        val digitRating = findViewById<TextView>(R.id.digitRating)
+        val ratingApp = findViewById<RatingBar>(R.id.ratingApp)
+        val reviewGradeCount = findViewById<TextView>(R.id.reviewGradeCount)
+        val reviewsList = findViewById<LinearLayout>(R.id.reviewsList)
+        val button = findViewById<Button>(R.id.button)
+
+        dataModel.apply {
+            Glide.with(this@MainActivity)
+                .load(image).priority(Priority.HIGH).dontAnimate()
+                .into(imageApp)
+            Glide.with(this@MainActivity)
+                .load(logo).priority(Priority.HIGH).dontAnimate()
+                .into(iconApp)
+            nameApp.text = name
+            ratingAppUpper.rating = rating
+            gradeCount.text = gradeCnt
+
+            tagsList.removeAllViews()
+            for (tag in tags) {
+                LayoutInflater.from(this@MainActivity).inflate(R.layout.layout_tag, null).apply {
+                    findViewById<TextView>(R.id.tag).text = tag
+                    tagsList.addView(this)
+                }
+            }
+
+            descApp.text = description
+            digitRating.text = rating.toString()
+            ratingApp.rating = rating
+            reviewGradeCount.text = "$gradeCnt Reviews"
+
+            reviewsList.removeAllViews()
+            for (review in reviews) {
+                LayoutInflater.from(this@MainActivity).inflate(R.layout.layout_review, null).apply {
+                    findViewById<TextView>(R.id.userName).text = review.userName
+                    findViewById<TextView>(R.id.date).text = review.date
+                    findViewById<TextView>(R.id.message).text = review.message
+                    Glide.with(this@MainActivity)
+                        .load(review.userImage).priority(Priority.HIGH).dontAnimate()
+                        .into(findViewById(R.id.userImage))
+                    reviewsList.addView(this)
+                }
+            }
+
+            button.text = action.name
+        }
+
     }
 }
